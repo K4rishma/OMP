58d0e7d1d75a561e7b4a9b79f7e967188efc10ae
c4sper <k.kumar-3@student.tudelft.nl>
2019-10-24 19:27:33 +0200

    optimised the code for calculations, map changed for correlation

--- correlation2.m
+++ correlation2.m
@@ -10,23 +10,25 @@ x_disp = zeros(kernels,1);
 y_disp = zeros(kernels,1);
 max_value = zeros(kernels,1);
 
-meanIm1 = mean( sum(sum(image1))./b_size.^2 );
-meanIm2 = mean(mean(mean(image2)));
-image1 = image1 - meanIm1;
-%image2 = image2 - meanIm2;
+meanIm1 = sum(sum(image1))./b_size.^2;
+image1 = image1;
 
 D = karishma_Dcorrelation(image2, b_size);
 y = reshape(image1, [], 1, n_avg, kernels);
 corrmap = zeros(b_size + 1, b_size + 1,kernels);
 
-for k = 1:kernels
+masked_kernels = 1:kernels;
+masked_kernels(masked_indices) = [];
+
+for k = masked_kernels(1) : masked_kernels(end)
 cor = zeros(b_size + 1, b_size + 1);
 
 for n = 1 : n_avg
     im2 = permute(D(:,:,n,k), [2,1,3,4]); 
-%     temp = ((D(:,:,n,k).')./std2(image2(:,:,n,k)))*y(:,1,n,k)./std2(image1(:,:,n,k));  % not taking absolute
-    temp = ((D(:,:,n,k)./std(D(:,:,n,k))).')*y(:,1,n,k)./std2(image1(:,:,n,k));    
-%ver = xcorr2(image2(:,:,n,k)./std2(image2(:,:,n,k)),image1(:,:,n,k)./std2(image1(:,:,n,k)));
+    %temp = ((D(:,:,n,k)./std(D(:,:,n,k))).')*y(:,1,n,k)./std2(image1(:,:,n,k));    
+    %ver = xcorr2(image2(:,:,n,k)./std2(image2(:,:,n,k)),image1(:,:,n,k)./std2(image1(:,:,n,k)));
+    Im1 = ( y(:,1,n,k) - mean(y(:,1,n,k)) )./std(y(:,1,n,k));
+    temp = ((D(:,:,n,k)).')*Im1;  
     temp = reshape(temp, b_size+1, b_size+1);  
     temp = temp./(size(image1,1)*size(image1,2));
     cor = cor + temp;
